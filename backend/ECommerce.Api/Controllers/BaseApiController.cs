6c4f4b91da9a6786d472fc7c588a0710b0f9817f
VictorLCosta <victorlc2019@outlook.com>
2022-09-16 17:02:19 -0300

    Backend - Base Api Controller

---
+++ backend/ECommerce.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();
+    }
+}
