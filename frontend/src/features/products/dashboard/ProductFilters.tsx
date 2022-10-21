b3b9b0cff34c5bc069fd300b0cc8d386416006fc
VictorLCosta <victorlc2019@outlook.com>
2022-10-21 19:49:58 -0300

    Frontend - Product filter list

--- frontend/src/features/products/dashboard/ProductFilters.tsx
+++ frontend/src/features/products/dashboard/ProductFilters.tsx
@@ -1,6 +1,10 @@
+import { FilterAltOutlined } from '@mui/icons-material';
+import { LoadingButton } from '@mui/lab';
+import { Divider, ListItemText, MenuItem, MenuList, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles'
 
-const Div = styled("div")(props => ({
+const Main = styled("div")(props => ({
+    position: 'sticky',
     display: 'block',
     width: '100%',
     background: 'var(--white)',
@@ -17,9 +21,27 @@ const Div = styled("div")(props => ({
 
 const ProductFilters = () => {
     return (
-        <Div>
-            Menu
-        </Div>
+        <Main>
+
+            <Typography variant='h4' mb={2}>
+                <FilterAltOutlined /> Filters
+            </Typography>
+
+            <MenuList>
+                Ainda não sei o que colocar aqui :)
+            </MenuList>
+
+            <Divider sx={{ marginBottom: '2rem' }} />
+
+            <LoadingButton
+                loadingPosition="start"
+                variant="outlined"
+                sx={{ width: '100%', fontSize: '1.2rem' }}
+            >
+                Clear filters
+            </LoadingButton>
+
+        </Main>
     )
 }
 
