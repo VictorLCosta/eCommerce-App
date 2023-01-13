71ddd233e6621fd372e96f1045c13f1f19a44e4e
VictorLCosta <victorlc2019@outlook.com>
2023-01-13 15:06:49 -0300

    feat(frontend): adds react-query config to the project

---
+++ frontend/src/lib/react-query.ts
@@ -0,0 +1,34 @@
+import { QueryClient } from "react-query";
+
+import type { AxiosError } from "axios";
+import type {
+  DefaultOptions,
+  UseQueryOptions,
+  UseMutationOptions,
+} from "react-query";
+import type { Promisable } from "type-fest";
+
+const queryConfig: DefaultOptions = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retry: false,
+  },
+};
+
+export const queryClient = new QueryClient({ defaultOptions: queryConfig });
+
+export type ExtractFnReturnType<FnType extends (...args: any) => any> =
+  Promisable<ReturnType<FnType>>;
+
+export type QueryConfig<QueryFnType extends (...args: any) => any> = Omit<
+  UseQueryOptions<ExtractFnReturnType<QueryFnType>>,
+  "queryKey" | "queryFn"
+>;
+
+export type MutationConfig<MutationFnType extends (...args: any) => any> =
+  UseMutationOptions<
+    ExtractFnReturnType<MutationFnType>,
+    AxiosError,
+    Parameters<MutationFnType>[0]
+  >;
