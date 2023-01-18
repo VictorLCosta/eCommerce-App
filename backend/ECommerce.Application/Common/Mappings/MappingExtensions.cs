68243b9b9f8bbd7e3f95651a08750b39d5ab3130
VictorLCosta <victorlc2019@outlook.com>
2023-01-17 22:38:23 -0300

    feat(backend): adds MappingExtensions for paginated lists

---
+++ backend/ECommerce.Application/Common/Mappings/MappingExtensions.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using ECommerce.Application.Common.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Application.Common.Mappings
+{
+    public static class MappingExtensions
+    {
+        public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize) where TDestination : class
+            => PaginatedList<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
+
+        public static Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider configuration) where TDestination : class
+            => queryable.ProjectTo<TDestination>(configuration).AsNoTracking().ToListAsync();
+            
+    }
+}
