f2704e60b145cc823169b3f23d57819534deeca0
VictorLCosta <victorlc2019@outlook.com>
2023-01-20 10:56:12 -0300

    feat(backend): adds ResponseCacheService for caching results

---
+++ backend/ECommerce.Application/Common/Interfaces/IResponseCacheService.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Common.Interfaces
+{
+    public interface IResponseCacheService
+    {
+        Task CacheResponseAsync(string cacheKey, object response, TimeSpan timeToLive);
+        Task<string> GetCachedResponse(string cacheKey);
+    }
+}
