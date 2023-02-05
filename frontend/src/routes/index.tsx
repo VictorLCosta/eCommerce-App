eed0fefac3235e97ffca60aca89c5121db7cef73
VictorLCosta <victorlc2019@outlook.com>
2023-02-05 12:31:29 -0300

    feat(frontend): adds NotFound route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,12 +2,16 @@
 
 import { useRoutes } from "react-router-dom";
 
+import { NotFound } from "@/features/misc";
+
 import { publicRoutes } from "./public";
 
 export function AppRoutes() {
+  const commonRoutes = [{ path: "*", element: <NotFound /> }];
+
   const routes = publicRoutes;
 
-  const element = useRoutes([...routes]);
+  const element = useRoutes([...routes, ...commonRoutes]);
 
   return <>{element}</>;
 }
