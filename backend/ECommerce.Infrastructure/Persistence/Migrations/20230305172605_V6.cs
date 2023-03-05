1cfab80eca0cd9f8e9c8501f5e641433e02277b3
VictorLCosta <victorlc2019@outlook.com>
2023-03-05 14:50:24 -0300

    feat(backend): adds image migrations

---
+++ backend/ECommerce.Infrastructure/Persistence/Migrations/20230305172605_V6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class V6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "varchar(150)", nullable: true),
+                    Url = table.Column<string>(type: "varchar(150)", nullable: true),
+                    Type = table.Column<string>(type: "varchar(150)", nullable: true),
+                    IsMain = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
